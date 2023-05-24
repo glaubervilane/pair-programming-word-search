d43918f49871bb41cdba7b2bccc0114acc6af8f7
Glauber Marques <glauberfmarques@gmail.com>
2023-05-23 18:16:22 -0600

    Update wordsearch to find words horizontaly or vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  // Check horizontally
+  for (const row of letters) {
+    const horizontalWord = row.join('');
+    if (horizontalWord.includes(word)) {
+      return true;
     }
-}
-
-module.exports = wordSearch
+  }
+  
+  // Check vertically
+  const numRows = letters.length;
+  const numCols = letters[0].length;
+  
+  for (let col = 0; col < numCols; col++) {
+    let verticalWord = '';
+    for (let row = 0; row < numRows; row++) {
+      verticalWord += letters[row][col];
+    }
+    if (verticalWord.includes(word)) {
+      return true;
+    }
+  }
+  
+  return false;
+};
+  
+module.exports = wordSearch;
+  
